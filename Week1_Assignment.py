805679b61d4b00a16093876d81ee816c43cc773d
germanwalakarishma <germanwalakarishma@gmail.com>
2020-04-28 12:25:20 +0530

    updating question to answer

--- Week1_Assignment.py
+++ Week1_Assignment.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
 
-# Question 1
+# Answer 1
 consecutiveNaturalNum = np.arange(18).reshape(6, 3)
 print("consecutive Natural Numbers::\n{}".format(consecutiveNaturalNum))
 
-# Question 2
+# Answer 2
 df = pd.read_excel("Week 1 Dataset.xlsx")
 
 df2 = df.rename(columns={"Name": "FirstName", "Income per month": "Income",
@@ -13,7 +13,7 @@ df2 = df.rename(columns={"Name": "FirstName", "Income per month": "Income",
 print(df2)
 
 
-# Question 3
+# Answer 3
 
 df.rename(columns={"Name": "FirstName", "Income per month": "Income",
                    "State": "State_name", "Age": "Age_Num", "Sex": "Gender",
@@ -21,27 +21,27 @@ df.rename(columns={"Name": "FirstName", "Income per month": "Income",
 print(df)
 print(df[["FirstName", "State_name", "Age_Num"]])
 
-# Question 4
+# Answer 4
 df4 = pd.read_excel("Week 1 Dataset.xlsx")
 df4 = df4.iloc[0:10, -2:]
 print("iloc\n", df4)
 
-# Question 5
+# Answer 5
 df5 = pd.read_excel("Week 1 Dataset.xlsx")
 df5 = df5.iloc[3:10, 1:5]
 print("iloc\n", df5)
 
-# Question 6
+# Answer 6
 df6 = pd.read_excel("Week 1 Dataset.xlsx")
 # print("Sum :: ", sum(df6["Number of siblings"]))
 print("Count :: \n", df6["Number of siblings"].value_counts())
 
-# Question 7
+# Answer 7
 # Answer -> Dependant variables - number of siblings
 #           Independant variables - Income per month,age
 
 
-# Question 8
+# Answer 8
 # Each square shows correlation between variables in the dataset.
 # As correlation value ranges from -1 to +1 so
 # value close to 1 means more positively correlated the variables are,
